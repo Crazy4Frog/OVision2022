8d46febbfe193aa7a0c7078c155a6658e7126df5
shameoff <eshamov030316@gmail.com>
2022-04-17 01:40:37 +0700

    # Conflicts: #	static/js/css-element-queries-1.2.1/.gitignore #	static/js/css-element-queries-1.2.1/.npmignore #	static/js/css-element-queries-1.2.1/LICENSE #	static/js/css-element-queries-1.2.1/README.md #	static/js/css-element-queries-1.2.1/css-element-queries.d.ts #	static/js/css-element-queries-1.2.1/index.js #	static/js/css-element-queries-1.2.1/package.json #	static/js/css-element-queries-1.2.1/src/ElementQueries.d.ts #	static/js/css-element-queries-1.2.1/src/ElementQueries.js #	static/js/css-element-queries-1.2.1/src/ResizeSensor.d.ts #	static/js/css-element-queries-1.2.1/src/ResizeSensor.js #	static/js/css-element-queries-1.2.1/tests/demo.css #	static/js/css-element-queries-1.2.1/tests/demo.html #	static/js/css-element-queries-1.2.1/tests/demo.js #	static/js/css-element-queries-1.2.1/tests/late-trigger.html #	static/js/css-element-queries-1.2.1/tests/mutation/app.js #	static/js/css-element-queries-1.2.1/tests/mutation/app.ts #	static/js/css-element-queries-1.2.1/tests/mutation/index.html #	static/js/css-element-queries-1.2.1/tsconfig.json #	templates/index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,50 @@
-if(navigator.webkitGetUserMedia!=null) {
-    var options = {
-        video:true,
-        audio:true
-    };
+let container = document.querySelector('div#interactive')
+let canvas = document.querySelector('canvas#onVideoCanvas');
+let invisibleCanvas = document.querySelector('canvas#invisible')
+let video = document.querySelector('video');
+let ctx = canvas.getContext('2d');
+let invisibleContext = invisibleCanvas.getContext('2d')
+let isCanvasResized = false
+new ResizeSensor(container, Program)
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+        video.srcObject = stream;
+    }).catch(function (error) {
+        console.log("Something went wrong!");
+    })
+} else {
+    console.log("getUserMedia not supported!")
+}
 
-    // запрашиваем доступ к веб-камере
-    navigator.webkitGetUserMedia(options,
-        function(stream) {
-            // получаем тег video
-            var video = document.querySelector('video');
-            // включаем поток в магический URL
-            video.src = window.webkitURL.createObjectURL(stream);
-        },
-        function(e) {
-            console.log("error happened");
+video.addEventListener('play', function () {
+    let $this = this; //cache
+    (function loop() {
+        if (!$this.paused && !$this.ended) {
+            invisibleContext.drawImage($this, 0, 0);
+            setTimeout(loop, 1000 / 60); // drawing at 30fps
         }
-    );
+    })();
+}, false);
+
+
+function resizeCanvas() {
+    invisibleCanvas.width = container.offsetWidth;
+    invisibleCanvas.height = container.offsetHeight;
+    canvas.width = container.offsetWidth;
+    canvas.height = container.offsetHeight;
+    isCanvasResized = true;
+}
+function drawRectangle(point1, point3, color){
+    ctx.beginPath()
+    ctx.strokeStyle = color;
+    ctx.moveTo(point1.x, point1.y);
+    ctx.lineTo(point3.x, point1.y);
+    ctx.lineTo(point3.x, point3.y);
+    ctx.lineTo(point1.x, point3.y);
+    ctx.lineTo(point1.x, point1.y);
+    ctx.stroke();
+}
+function Program(){
+    resizeCanvas();
+    // drawRectangle({x: 50, y: 30}, {x: 400, y: 200}, 'red')
 }
