6b0e77941102c4a64677979c4584386198c63af0
shameoff <eshamov030316@gmail.com>
2022-04-16 20:38:15 +0700

    Еще какие-то изменения

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 let container = document.querySelector('div#interactive')
-let canvas = document.querySelector('canvas');
+let canvas = document.querySelector('canvas#onVideoCanvas');
+let invisibleCanvas = document.querySelector('canvas#invisible')
 let video = document.querySelector('video');
 let ctx = canvas.getContext('2d');
+let invisibleContext = invisibleCanvas.getContext('2d')
 let isCanvasResized = false
 new ResizeSensor(container, Program)
-
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
         video.srcObject = stream;
@@ -14,7 +15,21 @@ if (navigator.mediaDevices.getUserMedia) {
 } else {
     console.log("getUserMedia not supported!")
 }
+
+video.addEventListener('play', function () {
+    let $this = this; //cache
+    (function loop() {
+        if (!$this.paused && !$this.ended) {
+            invisibleContext.drawImage($this, 0, 0);
+            setTimeout(loop, 1000 / 60); // drawing at 30fps
+        }
+    })();
+}, false);
+
+
 function resizeCanvas() {
+    invisibleCanvas.width = container.offsetWidth;
+    invisibleCanvas.height = container.offsetHeight;
     canvas.width = container.offsetWidth;
     canvas.height = container.offsetHeight;
     isCanvasResized = true;
